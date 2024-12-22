ceb7299ae60066cbb2b546f761be364f32aea09a
Luiie <dbsldkfls@ewhain.net>
2024-12-22 11:06:21 +0900

    data transfer between components

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -4,7 +4,11 @@ import './Lists.css';
 const Lists = (props) => {
   console.log(props.data);
   return (
-    <ul className="lists">
+    <ul className="lists">{
+        props.data.map((listData) => {
+            return <li key={listData.id}>{listData.text}</li>;
+        })
+    }
     </ul>
   );
 }
