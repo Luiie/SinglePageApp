285be38242ddd3ab4bdd2e7647e7713652461b69
Luiie <dbsldkfls@ewhain.net>
2024-12-28 21:12:18 +0900

    add new input to list

--- src/components/NewList/NewList.js
+++ src/components/NewList/NewList.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NewList.css';
 
 const NewList = (props) => {
+    const [enteredText, setEnteredText] = useState();
+
     const addListHandler = (event) => {
         event.preventDefault();
 
         const newList = {
-            id: 4,
-            text: "Add new list!"
-        }
+            id: 0,
+            text: enteredText
+        };
+
+        setEnteredText('');
+
         props.onAddList(newList);
-    }
+    };
+
+    const textHandler = (event) => {
+        setEnteredText(event.target.value);
+    };
+    
     return (
         <form className='new-list' onSubmit={addListHandler}>
-            <input type='text' />
+            <input type='text' value={enteredText} onChange={textHandler}/>
             <button type='submit'>Add</button>
         </form>
     );
